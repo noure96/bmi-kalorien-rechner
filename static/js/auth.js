f893543a7fba31cb892674b5371b756d8680a982
Nour-Eddine Bekkali <noureddine.bekkali@gmail.com>
2026-01-21 04:24:19 +0100

    projekt.js bearbeitet

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,17 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const token = localStorage.getItem("token");
 
-    // Seiten die Login brauchen
-    const protectedPages = [
-        "index.html",
-        "bmirechner.html",
-        "profil.html",
-        "daten.html"
-    ];
-
-    const currentPage = location.pathname.split("/").pop();
-
-    if (protectedPages.includes(currentPage) && !token) {
+    if (!token) {
         location.href = "login.html";
     }
 });
